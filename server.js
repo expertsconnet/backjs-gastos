674248b1e2a2a7905778732c2f77052a87a71374
expertsconnet <expertsconnet@gmail.com>
2025-08-06 13:34:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'http://127.0.0.1:5173',
-    'https://itecsolutions.onrender.com'
+    'http://127.0.0.1:5500',
+    'https://itecsolutions.onrender.com
+    '
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
